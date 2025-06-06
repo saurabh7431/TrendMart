ca34a9f4be42a184422cf7be59583e63b170f3b2
Saurabh Singh <saurabht019@gmail.com>
2025-06-06 22:54:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ app.use('/cart' , cartRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
+
 
